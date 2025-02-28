90133bd54d12e1610a180596349174ed5bc420c7
unknown <alaminhossen176466@gmail.com>
2025-02-28 09:49:25 +0600

    added new tailwind or daisyui style

--- jsfolder/login.js
+++ jsfolder/login.js
@@ -1,6 +1,30 @@
 
 
-document.getElementById('login-btn').addEventListener('click', function(){
-console.log('Hello')
+document.getElementById('login-btn').addEventListener('click', function(event){
+event.preventDefault()
+
+const accountNumber = document.getElementById("account-number").value
+const pin = document.getElementById('pin').value;
+const convertPin = parseInt(pin)
+if(accountNumber.length === 11) {
+
+
+if( convertPin === 1234) {
+    window.location.href="main.html"
+}
+else{
+    alert("Pin tikh nai")
+}
+
+}
+else{
+ alert("need value account Number")
+}
+
+
+
 })
+
+
+
 
